714a62fc4e707d8951c5f29ebdaf6ab833dcfb97
Bethany Blakely <bethanyblakely@Bethanys-MacBook-Pro.local>
2018-12-08 14:23:36 -0500

    Clean up of plots, for AGU poster

--- CompareData.R
+++ CompareData.R
@@ -12,47 +12,56 @@ a.chg<-read.csv('EGtoDC_Data/EGtoDC_Albedo_Change.csv')
 #st.dc<-read.csv('EGtoDC_Data/EGtoDC_ST_Modern.csv')
 st.chg<-read.csv('EGtoDC_Data/EGtoDC_ST_Change.csv')
 
-transp.chg<-read.csv('pls.fia.transp.diff.csv') #Actually no time to subset this by taxa and make it phenological
+#transp.chg<-read.csv('pls.fia.transp.diff.csv') #Actually no time to subset this by taxa and make it phenological
 
 ##All the plots
 
-###Albedo
+par(mfrow=c(1,3), mar=c(4,5.5,4,0.1))
+
+###Albedo####
 ed.alb<--databin.ed[,,1]
 jules.alb<--databin.jules[,,1]
 
+ylab.exp<-expression(bold(Delta~Albedo))
 
 par(mfrow=c(1,3))
-plot(rowMeans(ed.alb), type='l', ylim=c(-0.07, 0.07), main='ED', lwd=2, ylab="Albedo", xlab="Month")
+plot(rowMeans(ed.alb), type='l', ylim=c(-0.07, 0.07), main='ED2', lwd=2, 
+     ylab=ylab.exp, xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:ncol(ed.alb)){
-  lines(ed.alb[,i], lwd=0.5, col='gray')
-  lines(rowMeans(ed.alb), lwd=2)
+  lines(ed.alb[,i], lwd=1, col='dark gray')
+  lines(rowMeans(ed.alb), lwd=4, col='blue')
 }
 abline(h=0)
+box(lwd=4)
 
 #colvec=c("red","orange","yellow","green","blue")
-plot(rowMeans(jules.alb[,c(1:3,5)]), type='l', ylim=c(-0.07, 0.07), main='JULES', lwd=2, ylab="Albedo", xlab="Month")
+plot(rowMeans(jules.alb[,c(1:3,5)]), type='l', ylim=c(-0.07, 0.07), main='JULES', lwd=2, 
+     ylab="", xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:ncol(jules.alb)){
   if(i !=4){
-    lines(jules.alb[,i], lwd=0.5, col='gray')
-    lines(rowMeans(jules.alb[,c(1:3,5)]), lwd=2)
+    lines(jules.alb[,i], lwd=1, col='dark gray')
+    lines(rowMeans(jules.alb[,c(1:3,5)]), lwd=4, col='blue')
   }
 }
 abline(h=0)
+box(lwd=4)
 
-plot(colMeans(a.chg[2:13], na.rm=TRUE), type='l',ylim=c(-0.07, 0.07), main='Data', lwd=2, ylab="Albedo", xlab="Month")
+plot(colMeans(a.chg[2:13], na.rm=TRUE), type='l',ylim=c(-0.07, 0.07), main='DATA', lwd=2, 
+     ylab="", xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:nrow(a.chg)){#ncol(a.chg)
-  lines(as.numeric(a.chg[i,2:13]), lwd=0.5, col='gray')
-  lines(colMeans(a.chg[2:13], na.rm=TRUE), lwd=2)
+  lines(as.numeric(a.chg[i,2:13]), lwd=1, col='dark gray')
+  lines(colMeans(a.chg[2:13], na.rm=TRUE), lwd=4, col='blue')
 }
 abline(h=0)
+box(lwd=4)
 
-dev.copy(png, filename='Figures/Albcompare.png', width=500, height=400); dev.off()
+dev.copy(png, filename='Figures/Albcompare.png', width=550, height=300); dev.off()
 
 
 #rm(list=setdiff(ls(), c("databin.jules","databin.ed","datasubset.jules",
 #"datasubset.ed","var.want","varset")))
 
-##AGU abstract metrics
+####AGU abstract metrics####
 
 colMeans(a.chg[2:13], na.rm=TRUE)->datmeans
 rowMeans(ed.alb)->edmeans
@@ -75,8 +84,8 @@ datmeans-jmeans
 
 (datmeans-jmeans)/jmeans #generally less than 3x higher in data, except for jan/feb where data is WAY higher
 1-((datmeans-jmeans)/datmeans) #generally 30-100% of data change captured by model; Jan/feb and november are exceptions
-
-##Surface temp
+#####
+####Surface temp####
 #SB law calcualtions
 
 sb<-5.67e-08
@@ -90,34 +99,43 @@ jules.st.chg<-(-(jules.eg.st-jules.dc.st))
 
 
 par(mfrow=c(1,3))
-plot(rowMeans(ed.st.chg), type='l', ylim=c(-3, 3), main='ED', lwd=2, ylab="Surface T", xlab="Month")
+
+ylab.exp<-expression(bold(Delta~Surface~Temperature~(degree*C)))
+
+plot(rowMeans(ed.st.chg), type='l', ylim=c(-3, 3), main='ED2', lwd=2, 
+     ylab=ylab.exp, xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:ncol(ed.st.chg)){
-  lines(ed.st.chg[,i], lwd=0.5, col='gray')
-  lines(rowMeans(ed.st.chg), lwd=2)
+  lines(ed.st.chg[,i], lwd=0.5, col='gray50')
+  lines(rowMeans(ed.st.chg), lwd=4, col='dark red')
 }
 abline(h=0)
+box(lwd=4)
 
 #colvec=c("red","orange","yellow","green","blue")
-plot(rowMeans(jules.st.chg[,c(1:3,5)]), type='l', ylim=c(-3, 3), main='JULES', lwd=2, ylab="Surface T", xlab="Month")
+plot(rowMeans(jules.st.chg[,c(1:3,5)]), type='l', ylim=c(-3, 3), main='JULES', lwd=2, 
+     ylab="", xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:ncol(jules.st.chg)){
   if(i !=4){
-    lines(jules.st.chg[,i], lwd=0.5, col='gray')
-    lines(rowMeans(jules.st.chg[,c(1:3,5)]), lwd=2)
+    lines(jules.st.chg[,i], lwd=0.5, col='gray50')
+    lines(rowMeans(jules.st.chg[,c(1:3,5)]), lwd=4, col='dark red')
   }
 }
 abline(h=0)
+box(lwd=4)
 
-plot(colMeans(st.chg[2:13], na.rm=TRUE), type='l',ylim=c(-3, 3), main='Data', lwd=2, ylab="Surface T", xlab="Month")
+plot(colMeans(st.chg[2:13], na.rm=TRUE), type='l',ylim=c(-3, 3), main='DATA', lwd=2, 
+     ylab="", xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:nrow(st.chg)){#ncol(st.chg)
-  lines(as.numeric(st.chg[i,2:13]), lwd=0.5, col='gray')
-  lines(colMeans(st.chg[2:13], na.rm=TRUE), lwd=2)
+  lines(as.numeric(st.chg[i,2:13]), lwd=0.5, col='gray50')
+  lines(colMeans(st.chg[2:13], na.rm=TRUE), lwd=4, col='dark red')
 }
 abline(h=0)
+box(lwd=4)
 
-dev.copy(png, filename='Figures/STcompare.png', width=500, height=400); dev.off()
+dev.copy(png, filename='Figures/STcompare.png', width=550, height=300); dev.off()
 
-
-#AGU metrics
+#####
+####AGU metrics####
 
 datstmeans<-colMeans(st.chg[2:13], na.rm=TRUE)
 edstmeans<- rowMeans(ed.st.chg)
@@ -128,27 +146,37 @@ edstmeans/datstmeans
 
 datstmeans-jstmeans
 jstmeans/datstmeans
-
+#####
 #Transpiration
 
 ed.transp<--databin.ed[,,8]
+
+y.exp<-expression(bold(Delta~Transpiration~(mm~h^-1)))
 par(mfrow=c(1,3))
-plot(rowMeans(ed.transp, na.rm=TRUE), type='l',ylim=c(-1e-5, 2e-5), main='ED', lwd=2, ylab="Transp", xlab="Month")
+plot(rowMeans(ed.transp*3600, na.rm=TRUE), type='l',ylim=c(-0.05,0.05), main='ED2', lwd=2, 
+     ylab=y.exp, xlab="Month", col='blue', cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in 1:ncol(ed.transp)){
-  lines(ed.transp[,i], lwd=0.5, col='gray')
-  lines(rowMeans(ed.transp), lwd=2)
+  lines(ed.transp[,i]*3600, lwd=1, col='dark gray')
+  lines(rowMeans(ed.transp)*3600, lwd=3, col=' dark slate blue')
 }
+abline(h=0)
+box(lwd=3)
 
 
 transp.dat<-read.csv("TranspChg_Sap.csv")[2:13]
 transp.ind<-read.csv("EGtoDC_ind.csv")[,2]
 
-dat.transp<-transp.dat[transp.ind,]
-
-plot(colMeans(dat.transp, na.rm=TRUE), type='l',ylim=c(-1e-5, 2e-5), main='DATA', lwd=2, ylab="Transp", xlab="Month")
+dat.transp<-transp.dat[transp.ind,]*3600
+plot(colMeans(dat.transp, na.rm=TRUE), type='l',ylim=c(-0.05, 0.05), main='DATA', 
+     lwd=2, ylab="", xlab="Month", cex.axis=1.5, cex.lab=2.5, font.axis=2, font.lab=2, cex.main=2)
 for(i in (1:nrow(dat.transp))){
-  lines(as.numeric(dat.transp[i,]), col='gray', lwd=0.5)
-  lines(colMeans(dat.transp), lwd=2)
+  lines(as.numeric(dat.transp[i,]), col='dark gray', lwd=0.5)
+  lines(colMeans(dat.transp), lwd=3, col='dark slate blue')
 }
+abline(h=0)
+box(lwd=3)
+dev.copy(png, filename='Figures/Transpcompare.png', width=550, height=300); dev.off()
 
-dev.copy(png, filename='Figures/Transpcompare.png', width=500, height=400); dev.off()
+
+
+
